88d2c6fdd530385ec80db8d606b4156e75d9d4b4
HamzaJirah <hamzajirah@gmail.com>
2018-09-03 21:15:15 +0100

    added code to be executed when function runs

--- event.js
+++ event.js
@@ -3,5 +3,5 @@ let button = document.querySelector("#button");
 button.addEventListener("click", clickButton);
 
 function clickButton(){
-  
+  paragraphText.innerText = "I am a named function from the click event"
 }
