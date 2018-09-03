a8806e2817eb0303e963c7d56a76e0167dd0c5f2
HamzaJirah <hamzajirah@gmail.com>
2018-09-03 21:24:49 +0100

    added a comment

--- event.js
+++ event.js
@@ -5,6 +5,7 @@ const paragraphText = document.getElementsByTagName("p")[0];
 //add eventListener
 button.addEventListener("click", clickButton);
 
+//create function for the above named event listener
 function clickButton(){
   paragraphText.innerText = "I am a named function from the click event"
 }
